f77b7605539ed8be08721bad8144ad1007672e82
Binary <rukywe@me.com>
2020-12-10 23:50:28 +0000

    fixed bug

--- app.js
+++ app.js
@@ -1,33 +1,27 @@
-// if (process.env.NODE_ENV !== "production") { 
-//   require('dotenv').config(); 
-// }  
-  require("dotenv").config(); 
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
 
-const express = require('express');
-const path = require('path');
-const mongoose = require('mongoose');
-const ejsMate = require('ejs-mate');
-const session = require('express-session');
-const flash = require('connect-flash');
-const ExpressError = require('./utils/ExpressError');
-const methodOverride = require('method-override');
-const port = 3030 || process.env.port;
-const passport = require('passport')
-const localStrategy = require('passport-local')
-const User = require('./models/user')
-const mongoSanitize = require("express-mongo-sanitize");
+const express = require("express");
+const path = require("path");
+const mongoose = require("mongoose");
+const ejsMate = require("ejs-mate");
+const session = require("express-session");
+const flash = require("connect-flash");
+const ExpressError = require("./utils/ExpressError");
+const methodOverride = require("method-override");
+const passport = require("passport");
+const LocalStrategy = require("passport-local");
+const User = require("./models/user");
 const helmet = require("helmet");
-
-const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
-
+const mongoSanitize = require("express-mongo-sanitize");
 const userRoutes = require("./routes/users");
 const campgroundRoutes = require("./routes/campgrounds");
 const reviewRoutes = require("./routes/reviews");
+
 const MongoDBStore = require("connect-mongo")(session);
 
-
-
-//"mongodb://localhost:27017/yelp-camp";
+const dbUrl = process.env.DB_URL || "mongodb://localhost:27017/yelp-camp";
 
 mongoose.connect(dbUrl, {
   useNewUrlParser: true,
@@ -39,7 +33,7 @@ mongoose.connect(dbUrl, {
 const db = mongoose.connection;
 db.on("error", console.error.bind(console, "connection error:"));
 db.once("open", () => {
-  console.log("Database Connected");
+  console.log("Database connected");
 });
 
 const app = express();
@@ -51,9 +45,11 @@ app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride("_method"));
 app.use(express.static(path.join(__dirname, "public")));
-
-app.use(mongoSanitize());
-
+app.use(
+  mongoSanitize({
+    replaceWith: "_",
+  })
+);
 const secret = process.env.SECRET || "thisshouldbeabettersecret!";
 
 const store = new MongoDBStore({
@@ -83,7 +79,6 @@ const sessionConfig = {
 app.use(session(sessionConfig));
 app.use(flash());
 app.use(helmet());
-
 
 const scriptSrcUrls = [
   "https://stackpath.bootstrapcdn.com/",
@@ -121,7 +116,7 @@ app.use(
         "'self'",
         "blob:",
         "data:",
-        "https://res.cloudinary.com/rukywe/",
+        "https://res.cloudinary.com/douqbebwk/", //SHOULD MATCH YOUR CLOUDINARY ACCOUNT!
         "https://images.unsplash.com/",
       ],
       fontSrc: ["'self'", ...fontSrcUrls],
@@ -129,10 +124,9 @@ app.use(
   })
 );
 
-
 app.use(passport.initialize());
-app.use(passport.session()); 
-passport.use(new localStrategy(User.authenticate()));
+app.use(passport.session());
+passport.use(new LocalStrategy(User.authenticate()));
 
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
@@ -144,8 +138,6 @@ app.use((req, res, next) => {
   next();
 });
 
-
-
 app.use("/", userRoutes);
 app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/reviews", reviewRoutes);
@@ -153,7 +145,6 @@ app.use("/campgrounds/:id/reviews", reviewRoutes);
 app.get("/", (req, res) => {
   res.render("home");
 });
-
 
 app.all("*", (req, res, next) => {
   next(new ExpressError("Page Not Found", 404));
@@ -165,10 +156,7 @@ app.use((err, req, res, next) => {
   res.status(statusCode).render("error", { err });
 });
 
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
   console.log(`Serving on port ${port}`);
 });
-
-
-
-
